acd33e3273db744cd48385c71844cbaf056433f0
Mubeenahmed143 <ahmedsheikhmubeen395@gmail.com>
2024-04-18 20:07:16 +0500

    done

--- SQL Server Assignment.sql
+++ SQL Server Assignment.sql
@@ -19,14 +19,15 @@ create proc sp_students
 
 (
 @choice NVARCHAR(50),
-@student_id INT ,
-@first_name VARCHAR(50),
-@last_name VARCHAR(50),
-@email VARCHAR(50),
-@gender VARCHAR(50),
-@address VARCHAR(100),
-@city VARCHAR(50)
+@student_id INT=null ,
+@first_name VARCHAR(50)=null,
+@last_name VARCHAR(50)=null,
+@email VARCHAR(50)=null,
+@gender VARCHAR(50)=null,
+@address VARCHAR(100)=null,
+@city VARCHAR(50)=null
 )
+
 
 as
 begin
@@ -69,14 +70,16 @@ begin
 
 	end
 
-	exec sp_students @choice='insert' ,@student_id = 2 , @first_name = 'aisha' , @last_name = 'ali',
-	@email='aish@gmail.com' , @gender = 'Female' , @address = 'norht nazimabad' , @city = 'islamabad';
+	drop proc sp_students
+
+	exec sp_students @choice='insert' , @first_name = 'murtaza' , @last_name = 'sadiq',
+	@email='aish@gmail.com' , @gender = 'male' , @address = 'korangi' , @city = 'karachi';
 
 	exec sp_students @choice='select'
-	select * from students;
+	
 
-	exec sp_students @choice='update' ,@student_id = 2 , @first_name = 'mubeen' , @last_name = 'ahmed',
-	@email='mubeen@gmail.com' , @gender = 'Male' , @address = 'musharraf' , @city = 'karachi';
+	exec sp_students @choice='update' ,@student_id = 4 , @first_name = 'ali' , @last_name = 'raza',
+	@email='ali@gmail.com' , @gender = 'Male' , @address = 'orangi' , @city = 'quetta';
 
 	exec sp_students @choice='delete' ,@student_id = 3 , @first_name = 'aisha' , @last_name = 'ali',
 	@email='aish@gmail.com' , @gender = 'Female' , @address = 'norht nazimabad' , @city = 'islamabad';
